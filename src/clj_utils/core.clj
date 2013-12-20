4417bd9185dd2f7923f619eab1199a4445783003
Pat Thomas <patthomassoftware@gmail.com>
2013-12-19 20:09:24 -0500

    A bunch of functions that I copied from some of my other projects

--- src/clj_utils/core.clj
+++ src/clj_utils/core.clj
@@ -1,6 +1,52 @@
 (ns clj-utils.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn randomly-call
+  "Given a list of functions, will randomly call one of them with args as the arguments."
+  [fn-list & args]
+  (let [handler (rand-nth fn-list)]
+    (apply handler args)))
+
+(defn deep-map [function coll]
+  (map (fn [c] (map function c))
+       coll))
+
+(defn rand-seq-from-pool
+  ([pool len]
+     (vec
+      (for [_ (range len)]
+        (rand-nth pool))))
+  ([pool len key]
+     (vec
+      (for [_ (range len)]
+        {key (rand-nth pool)}))))
+
+(declare swap-with-prev)
+
+(defn swap-with-next [coll n]
+  (if (>= n (-> coll count dec))
+    (swap-with-prev coll n)
+    (let [[first-part second-part] (split-at (inc n) coll)
+          truncated-first-part     (drop-last first-part)
+          truncated-second-part    (drop 1 second-part)
+          swapped-part             [(first second-part) (last first-part)]]
+      (concat truncated-first-part swapped-part truncated-second-part))))
+
+(defn swap-with-prev [coll n]
+  (if (<= n 0)
+    (swap-with-next coll n)
+    (let [n                        (if (>= n (count coll)) (-> coll count dec) n)
+          [first-part second-part] (split-at n coll)
+          truncated-first-part     (drop-last first-part)
+          truncated-second-part    (drop 1 second-part)
+          swapped-part             [(first second-part) (last first-part)]]
+      (concat truncated-first-part swapped-part truncated-second-part))))
+
+(defn randomly-swap [coll]
+  (randomly-call [swap-with-next swap-with-prev] coll (rand-int (count coll))))
+
+(defmacro force-repeat
+  "Like (repeat), but forces reevaluation of the function call. Useful for non-referentially-transparent functions."
+  [len fn-body]
+  `(for [_# (range ~len)]
+     ~fn-body))
+
