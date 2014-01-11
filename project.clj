03efac586cf5b03e666245411df0eb74259e4cda
Pat Thomas <patthomassoftware@gmail.com>
2014-01-11 15:29:11 -0500

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject clj-utils "0.1.0-SNAPSHOT"
+(defproject clj-utils "0.1.0"
   :description "A collection of various utility functions that I've found helpful in doing Clojure development."
   :url "http://github.com/pat-thomas/clj-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :lein-release {:scm :git
+                 :deploy-via :clojars}
   :dependencies [[org.clojure/clojure "1.5.1"]])
