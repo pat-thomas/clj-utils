cf1f129b599dc9a5136739ecc9e5c245c395a811
Pat Thomas <pthomas@relaynetwork.com>
2014-06-17 13:00:27 -0400

    singular function to determine if a sequence is of length 1

--- src/clj_utils/seq_utils.clj
+++ src/clj_utils/seq_utils.clj
@@ -4,3 +4,8 @@
   "Returns true if all elements in s are equal, false otherwise."
   [s]
   (-> s distinct count (= 1)))
+
+(defn singular?
+  "Returns true if s is of length 1, false otherwise"
+  [s]
+  (= (count s) 1))
