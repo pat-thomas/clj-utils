c8ed34411ee20685ff2dd76d343f006441555557
Pat Thomas <patthomassoftware@gmail.com>
2014-11-01 00:09:12 -0400

    Implement good-merge

--- src/clj_utils/lang_utils.clj
+++ src/clj_utils/lang_utils.clj
@@ -5,3 +5,21 @@
   `(defn ~name
      [anon#]
      (map ~impl anon#)))
+
+(defn remove-nil-values
+  "Removes values from a map that are nil."
+  [m]
+  (reduce (fn [acc k]
+            (if-let [v (get m k)]
+              (assoc acc k v)
+              acc))
+          {}
+          (keys m)))
+
+(defn good-merge
+  "Merges ms, but does not overwrite keys in m with nil values."
+  [& ms]
+  (reduce (fn [acc m]
+            (merge acc (remove-nil-values m)))
+          {}
+          ms))
