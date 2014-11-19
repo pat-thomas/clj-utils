3514accfd4c86efcbb95a9fe99630867158766ca
Pat Thomas <patthomassoftware@gmail.com>
2014-11-18 23:32:44 -0500

    defcurried macro to generate all possibly arities for a given function, which, when invoked, return partial applications.

--- src/clj_utils/macros.clj
+++ src/clj_utils/macros.clj
@@ -11,4 +11,23 @@
            (do (clojure.tools.logging/logf ~log-level (format ~log-str ~args-list))
                ~@body))))))
 
+(defn- args-list->args-lists-for-defcurried
+  [args-list]
+  (for [n (->> args-list count inc range)]
+    (vec (take n args-list))))
 
+(defn- gen-arity
+  [fn-name args-list]
+  `(~args-list
+    (partial ~fn-name ~@args-list)))
+
+(defmacro defcurried
+  [fn-name args-list & body]
+  (let [args-lists         (args-list->args-lists-for-defcurried args-list)
+        partial-args-lists (drop-last args-lists)
+        full-args-list     (last args-lists)]
+    `(defn ~fn-name
+       ~@(map (fn [partial-args-list]
+                (gen-arity fn-name partial-args-list))
+              partial-args-lists)
+       (~full-args-list ~@body))))
