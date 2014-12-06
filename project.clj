95e8ad14e19b2d5664db76de3ad708594721c96e
Pat Thomas <pthomas@relaynetwork.com>
2014-12-06 17:21:43 -0500

    Begin rebuilding, keep core functionality.

--- project.clj
+++ project.clj
@@ -5,5 +5,4 @@
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["releases" {:url   "http://github.com/pat-thomas/clj-utils"
                               :creds :gpg}]]
-  :dependencies [[org.clojure/clojure       "1.6.0"]
-                 [org.clojure/tools.logging "0.2.6"]])
+  :dependencies [[org.clojure/clojure       "1.6.0"]])
--- src/clj_utils/core.clj
+++ src/clj_utils/core.clj
@@ -1,1 +1,34 @@
 (ns clj-utils.core)
+
+(defmacro deftransform
+  [transform-name & body]
+  `(do ~@(concat
+          (->> body
+               (partition 2)
+               (map (fn [[fn-name# fn-body#]]
+                      `(defn ~fn-name#
+                         [~'$]
+                         ~fn-body#))))
+          (list `(defn ~transform-name
+                   [~'$]
+                   ~(concat (list '-> '$)
+                            (->> body
+                                 (partition 2)
+                                 (map first))))))))
+
+(defmacro defcurried
+  [fn-name args-list & body]
+  (let [args-list->args-lists-for-defcurried (fn [args-list]
+                                               (for [n (->> args-list count inc range)]
+                                                 (vec (take n args-list))))
+        gen-arity                            (fn [fn-name args-list]
+                                               `(~args-list
+                                                 (partial ~fn-name ~@args-list)))
+        args-lists                           (args-list->args-lists-for-defcurried args-list)
+        partial-args-lists                   (drop-last args-lists)
+        full-args-list                       (last args-lists)]
+    `(defn ~fn-name
+       ~@(map (fn [partial-args-list]
+                (gen-arity fn-name partial-args-list))
+              partial-args-lists)
+       (~full-args-list ~@body))))
